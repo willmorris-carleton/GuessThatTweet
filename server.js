96e9e74e982d2ccdac2e798d6b3b0723ad09952a
braedenbrooking <braedenbrooking@gmail.com>
2020-01-11 16:24:26 -0500

    Added parsing for tweets and a link builder

--- server.js
+++ server.js
@@ -45,5 +45,46 @@ function getTweetsByPersonSpecific(req,res,next) {
 
 }
 
+
+function parseTweets(tweetJson){
+	tweetsArray = [];
+	tweetStr = JSON.stringify(tweetJson);
+	//let index = tweetStr.search('js-tweet-text tweet-text')
+	while(true){
+		let index = tweetStr.search('js-tweet-text tweet-text')//66 characters before the tweet starts
+		if(index<0){
+			break;//If no more tweets available
+		}
+		
+		
+		tweetStr = tweetStr.slice(index+66);//Slices the json string to start right at the beginning of the current tweet
+		let endIndex = tweetStr.search('</p>');//This is always after a tweet
+		let newTweet = tweetStr.slice(0,endIndex);
+		tweetStr = tweetStr.slice(endIndex);//Slices off that tweet so that it can keep going
+		tweetsArray.push(newTweet);
+	}
+	
+	//This part goes through each tweets and gets rid of the html tags
+	for(let i=0; i<tweetsArray.length; i++){
+		while(true){
+			//Searches for additional pieces of inside html (@'s #'s etc.)
+			let htmlIndex = tweetsArray[i].search("<");
+			let htmlEndIndex = tweetsArray[i].search(">");
+			if(htmlIndex<0){
+				break;//If none found break
+			}
+			
+			tweetsArray[i] = tweetsArray[i].slice(htmlIndex, htmlEndIndex);
+		}
+	}
+	
+	return tweetsArray;
+	
+}
+
+function createLink(username){
+	return "https://twitter.com/i/search/timeline?f=live&q=(from%3A" + username + ")%20-filter%3Alinks%20-filter%3Areplies&src=typd"
+}
+
 app.listen(3000);
 console.log("Listening on port 3000");
